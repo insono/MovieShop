1bc8d474bee60d16c4b8cc97c35954f58943ced9
Andrew Yuan <andrew.yuan1997@gmail.com>
2021-07-18 23:27:00 -0700

    update

--- ApplicationCore/RepositoryInterfaces/IMovieRepository.cs
+++ ApplicationCore/RepositoryInterfaces/IMovieRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApplicationCore.Entities;
 
 namespace ApplicationCore.RepositoryInterfaces
 {
-    public interface IMovieRepository
-    {
-    }
+   public interface IMovieRepository: IAsyncRepository<Movie>
+   {
+       Task<IEnumerable<Movie>> GetTop30HighestRevenueMovies();
+   }
 }
--- ApplicationCore/RepositoryInterfaces/IUserRepository.cs
+++ ApplicationCore/RepositoryInterfaces/IUserRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApplicationCore.Entities;
 
 namespace ApplicationCore.RepositoryInterfaces
 {
-    interface IUserRepository
-    {
-    }
+   public interface IUserRepository: IAsyncRepository<User>
+   {
+       Task<User> GetUserByEmail(string email);
+   }
 }
